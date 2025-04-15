0df3e13ade991a7623177177754503c9e6b840e3
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-04-15 15:29:26 +0200

    [BONUS] Bonus completato

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let hello = process.argv[2]
+let name = process.argv[3]
+let surName = process.argv[4]
 
-console.log("Hello Boolean")
+console.log(hello, name, surName)
+
+// const x = parseInt(process.argv[2]);
+// const y = parseInt(process.argv[3]);
+
+// const somma = x + y;
+// const moltiplicazione = x * y;
+// const sottrazione = x - y;
+// const divisione = x / y;
+
+// console.log({somma, moltiplicazione, sottrazione, divisione})
