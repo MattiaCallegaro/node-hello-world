070e90128487e54e70345c2166b99b9a36adf77e
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-04-15 17:03:40 +0200

    [BONUS 2] add alternative

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
-// let hello = process.argv[2]
-// let name = process.argv[3]
-// let surName = process.argv[4]
-
+//METODO IF ELSE
 
 // ? = se questo è vero ---fai questo--- : = altrimenti
 //se la variabile hello che contiene proccess.argv[2] è vera fai questo proccess.argv[2] altimenti stringa
-let hello = process.argv[2] ? process.argv[2] : "Io"
-let name = process.argv[3] ? process.argv[3] : "Sono"
-let surName = process.argv[4] ? process.argv[4] : "Batman"
+// let hello = process.argv[2] ? process.argv[2] : "Io"
+// let name = process.argv[3] ? process.argv[3] : "Sono"
+// let surName = process.argv[4] ? process.argv[4] : "Batman"
 
+
+//METODO IF ELSE NORMALE
+
+ let hello = process.argv[2]
+ let name = process.argv[3]
+ let surName = process.argv[4]
+
+if(process.argv[2] ){
+    hello = process.argv[2]
+}
+else{
+    hello = "io"
+}
+
+if(process.argv[3] ){
+    name = process.argv[3]
+}
+else{
+    name = "Sono"
+}
+
+if(process.argv[4] ){
+    surName = process.argv[4]
+}
+else{
+    surName = "Batman"
+}
 console.log(hello, name, surName)
+
 
 
 
